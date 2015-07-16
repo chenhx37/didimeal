8314b98380cf6e25726d52117e7e275e70c7c07a
zZholdon <819219159@qq.com>
2015-07-16 22:58:57 +0800

    androidmodifynewbyczz

--- android/DiDiDaFan/src/com/example/dididafan/NewsActivity.java
+++ android/DiDiDaFan/src/com/example/dididafan/NewsActivity.java
@@ -36,55 +36,54 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
-
+//新闻界面的java文件，绑定了对应的xml文件
 public class NewsActivity extends Activity {
-	private ListView OIWlv;
-	private Button btn_message,btn_call;
-	private SimpleAdapter receiveadapter;
-	private SimpleAdapter pushdapter;
-	private List<Map<String,Object>> mDataList = new ArrayList<Map<String,Object>>();
-	private String baseurl = "http://ddmeal.sinaapp.com/";
-	private SimpleAdapter adapter;
+	 private ListView OIWlv;
+	 private Button btn_message,btn_call;
+	 private SimpleAdapter receiveadapter;
+	 private SimpleAdapter pushdapter;
+	 private List<Map<String,Object>> mDataList = new ArrayList<Map<String,Object>>();
+	 private String baseurl = "http://ddmeal.sinaapp.com/";
+	 private SimpleAdapter adapter;
+	//初始化变量
+	 private String mNamestr;
+	 private String lasttime;
+	 private String flag;
 	
-	private String mNamestr;
-	private String lasttime;
-	private String flag;
-	
-	
-	Handler handler = new Handler(){
-		public void handleMessage(android.os.Message msg){
-
-				if(msg.what==6){
+	 Handler handler = new Handler(){
+		 public void handleMessage(android.os.Message msg){
+				 if(msg.what==6){
 					//update listview
-					OIWlv.setAdapter(adapter);
-        			adapter.notifyDataSetChanged();
-        			Toast.makeText(getApplicationContext(), "消息刷新成功",Toast.LENGTH_SHORT).show();
-				}
-				
+					 OIWlv.setAdapter(adapter);
+        			 adapter.notifyDataSetChanged();
+        			 Toast.makeText(getApplicationContext(), "消息刷新成功",Toast.LENGTH_SHORT).show();
+				 }				
 			};
 		};
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.messagepage);
-		btn_message = (Button)findViewById(R.id.btn_message);
-		btn_call = (Button)findViewById(R.id.btn_call);
-		btn_message.setOnClickListener(onClicker);
-		btn_call.setOnClickListener(onClicker);
-		//显示订单的listview
-		OIWlv = (ListView)findViewById(R.id.MessagePagelistView);
-		mDataList.clear();
-		adapter = new SimpleAdapter(this,mDataList,R.layout.itemdetail,new String[]{"showitem"},new int[]{R.id.showitemNA});
-		OIWlv.setAdapter(adapter);
+	 protected void onCreate(Bundle savedInstanceState) {
+		 super.onCreate(savedInstanceState);
+		 setContentView(R.layout.messagepage);
+		 btn_message = (Button)findViewById(R.id.btn_message);
+		 btn_call = (Button)findViewById(R.id.btn_call);
+		 btn_message.setOnClickListener(onClicker);
+		 btn_call.setOnClickListener(onClicker);
+		 //显示订单的listview
+		 OIWlv = (ListView)findViewById(R.id.MessagePagelistView);
+		 mDataList.clear();
+		 adapter = new SimpleAdapter(this,mDataList,R.layout.itemdetail,new String[]{"showitem"},new int[]{R.id.showitemNA});
+		 OIWlv.setAdapter(adapter);
 		
-		DB snappydb;
-		try {
-			
+		 DB snappydb;
+		 try {
+
+		 
 			snappydb = DBFactory.open(getApplicationContext());
 			mNamestr = snappydb.get("username");
 			lasttime = snappydb.get("lasttime_msg");
 			flag = snappydb.get("flag_msg");
 			snappydb.close();
+			
 			
 		} catch (SnappydbException e) {
 			// TODO Auto-generated catch block
@@ -95,102 +94,114 @@ public class NewsActivity extends Activity {
 		
 		
 		
-		getAcceptOrder();
+		 getAcceptOrder();
 	}
 	
 	//跳转到主页
 	public void MessagePageMPBtnClick(View v){
-		Intent intent = new Intent();
-		intent.setClass(NewsActivity.this, MainPage.class);
-		startActivity(intent);
+		 Intent intent = new Intent();
+		 intent.setClass(NewsActivity.this, MainPage.class);
+		 startActivity(intent);
 	}
+	
 	//跳转到发布
 	public void MessagePagePublishBtnClick(View v){
-		Intent intent = new Intent();
-		intent.setClass(NewsActivity.this, PublishActivity.class);
-		startActivity(intent);
+		 Intent intent = new Intent();
+		 intent.setClass(NewsActivity.this, PublishActivity.class);
+		 startActivity(intent);
 	}
+	
 	//跳转到消息
-	public void MessagePageNewsBtnClick(View v){
-		Intent intent = new Intent();
-		intent.setClass(NewsActivity.this, NewsActivity.class);
-		startActivity(intent);
-	}
-	private OnClickListener onClicker = new OnClickListener() {
-		@Override
-		public void onClick(View v) {
-			switch (v.getId()) {
-			case R.id.btn_message:
-				btn_message.setTextColor(Color.parseColor("#df3031"));
-				btn_call.setTextColor(Color.WHITE);
-				btn_message.setBackgroundResource(R.drawable.baike_btn_pink_left_f_96);
-				btn_call.setBackgroundResource(R.drawable.baike_btn_trans_right_f_96);
-				//switchFragment(MESSAGE_FRAGMENT_TYPE);
-				mDataList.clear();
-				getAcceptOrder();
-				break;
-			case R.id.btn_call:
+	 public void MessagePageNewsBtnClick(View v){
+		 Intent intent = new Intent();
+		 intent.setClass(NewsActivity.this, NewsActivity.class);
+		 startActivity(intent);
+	 }
+	 
+	 private OnClickListener onClicker = new OnClickListener() {
+		 @Override
+		 public void onClick(View v) {
+			 switch (v.getId()) {
+			 case R.id.btn_message:
+				 btn_message.setTextColor(Color.parseColor("#df3031"));
+				 btn_call.setTextColor(Color.WHITE);
+				 btn_message.setBackgroundResource(R.drawable.baike_btn_pink_left_f_96);
+				 btn_call.setBackgroundResource(R.drawable.baike_btn_trans_right_f_96);
+				 //switchFragment(MESSAGE_FRAGMENT_TYPE);
+				 mDataList.clear();
+				 getAcceptOrder();
+				 break;
+			 case R.id.btn_call:
 				
-				btn_message.setTextColor(Color.WHITE);
-				btn_call.setTextColor(Color.parseColor("#df3031"));
-				btn_message.setBackgroundResource(R.drawable.baike_btn_trans_left_f_96);
-				btn_call.setBackgroundResource(R.drawable.baike_btn_pink_right_f_96);
-				//switchFragment(CALL_FRAGMENT_TYPE);	
-				mDataList.clear();
-				getPostOrder();
-				break;			
+				 btn_message.setTextColor(Color.WHITE);
+				 btn_call.setTextColor(Color.parseColor("#df3031"));
+				 btn_message.setBackgroundResource(R.drawable.baike_btn_trans_left_f_96);
+				 btn_call.setBackgroundResource(R.drawable.baike_btn_pink_right_f_96);
+				 //switchFragment(CALL_FRAGMENT_TYPE);	
+				 mDataList.clear();
+				 getPostOrder();
+				 break;			
 			}
 		}
 	};
+	
 	//从网上get到用户接收的订单并显示
 	public void getAcceptOrder(){
+		
 		try{			
             // 发送请求
             Thread hth = new Thread(){
             	@Override
             	public void run(){
-            		//先get到个人信息，再post上去
-            		//System.out.println("hello111 ");            		          		
-            		HttpResponse response;
-            		try{	            			
-            			String url = baseurl + "ddmeal/index/myMessage/";	
+            		 //先get到个人信息，再post上去
+            		 //System.out.println("hello111 ");            		          		
+            		 HttpResponse response;
+					 
+            		 try{	            			
+            			 String url = baseurl + "ddmeal/index/myMessage/";	
             			
-            			HttpGet httpRequest = new HttpGet(url);
-            			//System.out.println("hello222 ");
-            			//header
-            			Header headers = new BasicHeader("Content-type","application/x-www-form-urlencoded");          
-            			httpRequest.setHeader(headers);
-            			Header headers1 = new BasicHeader("Accept","text/plain");
-            			httpRequest.setHeader(headers1);
-            			Header headers2 = new  BasicHeader("cookie","username="+mNamestr);
-            			httpRequest.setHeader(headers2);
-            			// 将请求体内容加入请求中
-            			//httpRequest.setEntity(requestHttpEntity);
-            			// 需要客户端对象来发送请求
-            			HttpClient httpClient = new DefaultHttpClient();
-            			// 发送请求
-            			response = httpClient.execute(httpRequest);         			
-            			//System.out.println("hello333 ");   
-            			String nowstr = null;
-            	        nowstr = EntityUtils.toString(response.getEntity());   //获取字符串
-            	        System.out.println("hello "+ nowstr);   
-            	        JSONObject jsonObject;
-            	        try {
-            	        		jsonObject = new JSONObject(nowstr);
-            	        		String outstr = jsonObject.getString("myAcceptOrders");            	        		
-            	        		//System.out.println("hello555 "+outstr);
-            	        		//解析订单的数据数组
-            	        		JSONArray arr = new JSONArray(outstr);
-            	        		//System.out.println("长度shi"+arr.length());
-            	        		for(int i=0;i<arr.length();i++){
-            	        			JSONObject temp = (JSONObject)arr.get(i);
+            			 HttpGet httpRequest = new HttpGet(url);
+            			 //System.out.println("hello222 ");
+            			 //header
+            			 Header headers = new BasicHeader("Content-type","application/x-www-form-urlencoded");          
+            			 httpRequest.setHeader(headers);
+            			 Header headers1 = new BasicHeader("Accept","text/plain");
+            			 httpRequest.setHeader(headers1);
+            			 Header headers2 = new  BasicHeader("cookie","username="+mNamestr);
+            			 httpRequest.setHeader(headers2);
+						 
+            			 // 将请求体内容加入请求中
+            			 //httpRequest.setEntity(requestHttpEntity);
+            			 // 需要客户端对象来发送请求
+            			 HttpClient httpClient = new DefaultHttpClient();
+            			 // 发送请求
+            			 response = httpClient.execute(httpRequest);         			
+            			//System.out.println("hello333 ");  
+						
+            			 String nowstr = null;
+            	         nowstr = EntityUtils.toString(response.getEntity());   //获取字符串
+            	         System.out.println("hello "+ nowstr);   
+            	         JSONObject jsonObject;
+						 
+            	         try {
+            	        		 jsonObject = new JSONObject(nowstr);
+            	        		 String outstr = jsonObject.getString("myAcceptOrders");            	        		
+            	        		 //System.out.println("hello555 "+outstr);
+            	        		 //解析订单的数据数组
+            	        		 JSONArray arr = new JSONArray(outstr);
+            	        		 //System.out.println("长度shi"+arr.length());
+								 
+            	        		 for(int i=0;i<arr.length();i++){
+            	        			 JSONObject temp = (JSONObject)arr.get(i);
+									 
             	        			//fields里面才有订单详细信息
             	        			//可以直接在这里添加你想获取的数据
-            	        			String result = temp.getString("fields");
+            	        			 String result = temp.getString("fields");
             	        			//System.out.println("fields " + result);
             	        	
-            	        			JSONObject smalltemp;       			
-            	        			try {
+            	        			JSONObject smalltemp; 
+									
+            	        			 try {
             							smalltemp = new JSONObject(result);					
             							Map<String,Object> mMap = new HashMap<String,Object>();
             		        			mMap.put("content", "内容: "+smalltemp.getString("description"));
@@ -211,7 +222,7 @@ public class NewsActivity extends Activity {
             	        			//System.out.println("item是这样的"+mDataList.get(i).get("content").toString());
             	        		}
             	        		
-            	        		handler.obtainMessage(6).sendToTarget();
+            	        		 handler.obtainMessage(6).sendToTarget();
             	        		/*
             	        		OIWlv.setAdapter(adapter);
             	        		adapter.notifyDataSetChanged();
@@ -222,20 +233,23 @@ public class NewsActivity extends Activity {
             	        		e1.printStackTrace();
             	        	}           
 						//}							
-            	    }catch (Exception e){
+            	     }catch (Exception e){
 						e.printStackTrace();
-					}
+					 }
             		
-				}
+				 }
             };
+			
 			hth.start();
 			//System.out.println("hello666 ");     
-		}catch(Exception e){
-        e.printStackTrace();
-		}
+		 }catch(Exception e){
+          e.printStackTrace();
+		 }
 	}
+	
 	//从网上get到用户发送的订单并显示
 	public void getPostOrder(){
+		
 		try{			
             // 发送请求
             Thread hth = new Thread(){
@@ -244,6 +258,7 @@ public class NewsActivity extends Activity {
             		//先get到个人信息，再post上去
             		//System.out.println("hello111 ");            		          		
             		HttpResponse response;
+					
             		try{	            			
             			String url = baseurl + "ddmeal/index/myMessage/";	
             			
@@ -324,7 +339,7 @@ public class NewsActivity extends Activity {
 			hth.start();
 			System.out.println("hello666 ");     
 		}catch(Exception e){
-        e.printStackTrace();
+			e.printStackTrace();
 		}	
 	}
 }
